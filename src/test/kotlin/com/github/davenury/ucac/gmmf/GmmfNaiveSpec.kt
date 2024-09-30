e250d9b4330329e92980990567a81ff36a7dd53c
Kamil Jarosz <kjarosh256@gmail.com>
2024-09-30 16:15:18 +0200

    gmmf: Add naive spec

---
+++ src/test/kotlin/com/github/davenury/ucac/gmmf/GmmfNaiveSpec.kt
@@ -0,0 +1,193 @@
+package com.github.davenury.ucac.gmmf
+
+import com.github.davenury.ucac.gmmf.routing.EdgeMessage
+import com.github.davenury.ucac.gmmf.routing.ReachesMessage
+import com.github.davenury.ucac.gmmf.routing.VertexMessage
+import com.github.davenury.ucac.testHttpClient
+import com.github.davenury.ucac.utils.IntegrationTestBase
+import com.github.davenury.ucac.utils.TestApplicationSet
+import com.github.davenury.ucac.utils.TestLogExtension
+import com.github.kjarosh.agh.pp.graph.model.Permissions
+import com.github.kjarosh.agh.pp.graph.model.Vertex
+import com.github.kjarosh.agh.pp.graph.model.VertexId
+import com.github.kjarosh.agh.pp.graph.model.ZoneId
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.slf4j.LoggerFactory
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import strikt.assertions.isFalse
+import strikt.assertions.isTrue
+
+@ExtendWith(TestLogExtension::class)
+class GmmfNaiveSpec : IntegrationTestBase() {
+    @BeforeEach
+    fun setup() {
+        System.setProperty("configFile", "application-integration.conf")
+    }
+
+    @Test
+    fun `naive reaches basic`(): Unit =
+        runBlocking {
+            apps = TestApplicationSet(mapOf("peerset0" to listOf("peer0", "peer1")))
+
+            logger.info("Adding v1")
+            addVertex("http://${apps.getPeer("peer0").address}/gmmf/graph/vertex?peerset=peerset0", "v1", Vertex.Type.GROUP)
+            logger.info("Adding v2")
+            addVertex("http://${apps.getPeer("peer1").address}/gmmf/graph/vertex?peerset=peerset0", "v2", Vertex.Type.GROUP)
+            logger.info("Adding v3")
+            addVertex("http://${apps.getPeer("peer0").address}/gmmf/graph/vertex?peerset=peerset0", "v3", Vertex.Type.GROUP)
+
+            logger.info("Adding v1->v2")
+            addEdge(
+                "http://${apps.getPeer("peer0").address}/gmmf/graph/edge?peerset=peerset0",
+                VertexId(ZoneId("peerset0"), "v1"),
+                VertexId(ZoneId("peerset0"), "v2"),
+                Permissions("01010"),
+            )
+            logger.info("Adding v2->v3")
+            addEdge(
+                "http://${apps.getPeer("peer0").address}/gmmf/graph/edge?peerset=peerset0",
+                VertexId(ZoneId("peerset0"), "v2"),
+                VertexId(ZoneId("peerset0"), "v3"),
+                Permissions("01010"),
+            )
+
+            val reachesMessage1 =
+                naiveReaches(
+                    "http://${apps.getPeer("peer0").address}/gmmf/naive/reaches?" +
+                        "from=peerset0:v1&to=peerset0:v3",
+                )
+            expectThat(reachesMessage1.reaches).isTrue()
+
+            val reachesMessage2 =
+                naiveReaches(
+                    "http://${apps.getPeer("peer0").address}/gmmf/naive/reaches?" +
+                        "from=peerset0:v1&to=peerset0:v2",
+                )
+            expectThat(reachesMessage2.reaches).isTrue()
+
+            val reachesMessage3 =
+                naiveReaches(
+                    "http://${apps.getPeer("peer0").address}/gmmf/naive/reaches?" +
+                        "from=peerset0:v2&to=peerset0:v3",
+                )
+            expectThat(reachesMessage3.reaches).isTrue()
+
+            val reachesMessage4 =
+                naiveReaches(
+                    "http://${apps.getPeer("peer0").address}/gmmf/naive/reaches?" +
+                        "from=peerset0:v3&to=peerset0:v1",
+                )
+            expectThat(reachesMessage4.reaches).isFalse()
+        }
+
+    @Test
+    @Disabled("TODO make parentId optional for 2PC")
+    fun `naive reaches two peersets`(): Unit =
+        runBlocking {
+            apps = TestApplicationSet(mapOf("peerset0" to listOf("peer0", "peer1"), "peerset1" to listOf("peer2", "peer3")))
+
+            addVertex("http://${apps.getPeer("peer0").address}/gmmf/graph/vertex?peerset=peerset0", "v1", Vertex.Type.GROUP)
+            addVertex("http://${apps.getPeer("peer1").address}/gmmf/graph/vertex?peerset=peerset0", "v2", Vertex.Type.GROUP)
+            addVertex("http://${apps.getPeer("peer2").address}/gmmf/graph/vertex?peerset=peerset1", "v3", Vertex.Type.GROUP)
+            addVertex("http://${apps.getPeer("peer3").address}/gmmf/graph/vertex?peerset=peerset1", "v4", Vertex.Type.GROUP)
+
+            var v2 = getVertex("http://${apps.getPeer("peer1").address}/gmmf/graph/vertex/v2?peerset=peerset0")
+            expectThat(v2.name).isEqualTo("v2")
+            expectThat(v2.type).isEqualTo(Vertex.Type.GROUP)
+
+            v2 = getVertex("http://${apps.getPeer("peer0").address}/gmmf/graph/vertex/v2?peerset=peerset0")
+            expectThat(v2.name).isEqualTo("v2")
+            expectThat(v2.type).isEqualTo(Vertex.Type.GROUP)
+
+            addEdge(
+                "http://${apps.getPeer("peer0").address}/gmmf/graph/edge?peerset=peerset0",
+                VertexId(ZoneId("peerset0"), "v1"),
+                VertexId(ZoneId("peerset0"), "v2"),
+                Permissions("01010"),
+            )
+            addEdge(
+                "http://${apps.getPeer("peer0").address}/gmmf/graph/edge?peerset=peerset0",
+                VertexId(ZoneId("peerset0"), "v2"),
+                VertexId(ZoneId("peerset1"), "v3"),
+                Permissions("01010"),
+            )
+            addEdge(
+                "http://${apps.getPeer("peer3").address}/gmmf/graph/edge?peerset=peerset1",
+                VertexId(ZoneId("peerset1"), "v3"),
+                VertexId(ZoneId("peerset1"), "v4"),
+                Permissions("01010"),
+            )
+
+            val reachesMessage1 =
+                naiveReaches(
+                    "http://${apps.getPeer("peer0").address}/gmmf/naive/reaches?" +
+                        "from=peerset0:v1&to=peerset1:v4",
+                )
+            expectThat(reachesMessage1.reaches).isTrue()
+
+            val reachesMessage2 =
+                naiveReaches(
+                    "http://${apps.getPeer("peer4").address}/gmmf/naive/reaches?" +
+                        "from=peerset0:v1&to=peerset1:v4",
+                )
+            expectThat(reachesMessage2.reaches).isTrue()
+        }
+
+    private suspend fun addVertex(
+        url: String,
+        name: String,
+        type: Vertex.Type,
+    ) {
+        testHttpClient.post<String>(url) {
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+            body = VertexMessage(name, type)
+        }
+    }
+
+    private suspend fun getVertex(url: String): VertexMessage =
+        testHttpClient.get<VertexMessage>(url) {
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+        }
+
+    private suspend fun addEdge(
+        url: String,
+        from: VertexId,
+        to: VertexId,
+        permissions: Permissions,
+    ) {
+        testHttpClient.post<HttpResponse>(url) {
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+            body = EdgeMessage(from, to, permissions)
+        }
+    }
+
+    private suspend fun getEdge(url: String): EdgeMessage =
+        testHttpClient.get<EdgeMessage>(url) {
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+        }
+
+    private suspend fun naiveReaches(url: String): ReachesMessage =
+        testHttpClient.post<ReachesMessage>(url) {
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+        }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(GmmfNaiveSpec::class.java)
+    }
+}
