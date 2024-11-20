e3bfa73b08079cd698468034607bfc9e0875ea56
Kamil Jarosz <kjarosh256@gmail.com>
2024-11-20 21:49:02 +0100

    gmmf: Add client

---
+++ src/main/kotlin/com/github/davenury/ucac/gmmf/client/GmmfClient.kt
@@ -0,0 +1,25 @@
+package com.github.davenury.ucac.gmmf.client
+
+import com.github.davenury.common.PeerAddress
+import com.github.davenury.ucac.gmmf.routing.ReachesMessage
+import com.github.davenury.ucac.httpClient
+import com.github.kjarosh.agh.pp.graph.model.VertexId
+import io.ktor.client.request.accept
+import io.ktor.client.request.post
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+
+class GmmfClient(peer: PeerAddress) {
+    private val urlBase: String = "http://${peer.address}"
+
+    suspend fun reaches(
+        from: VertexId,
+        to: VertexId,
+        ttl: Int,
+    ): ReachesMessage {
+        return httpClient.post<ReachesMessage>("$urlBase/gmmf/naive/reaches?from=$from&to=$to&ttl=$ttl") {
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+        }
+    }
+}
