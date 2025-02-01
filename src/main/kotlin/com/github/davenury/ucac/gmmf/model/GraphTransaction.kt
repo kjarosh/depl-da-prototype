5bdc82e5c81a1e502d8576bed241b23add12c39f
Kamil Jarosz <kjarosh256@gmail.com>
2025-02-01 21:52:54 +0100

    tests: Add sync operation for some tests

--- src/main/kotlin/com/github/davenury/ucac/gmmf/model/GraphTransaction.kt
+++ src/main/kotlin/com/github/davenury/ucac/gmmf/model/GraphTransaction.kt
@@ -2,6 +2,7 @@ package com.github.davenury.ucac.gmmf.model
 
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.core.JacksonException
 import com.github.davenury.common.objectMapper
 import com.github.kjarosh.agh.pp.graph.model.Edge
 import com.github.kjarosh.agh.pp.graph.model.EdgeId
@@ -39,7 +40,11 @@ sealed class GraphTransaction {
         val logger: Logger = LoggerFactory.getLogger("graph-tx")
 
         fun deserialize(content: String): GraphTransaction? {
-            return objectMapper.readValue(content, GraphTransaction::class.java)
+            try {
+                return objectMapper.readValue(content, GraphTransaction::class.java)
+            } catch (e: JacksonException) {
+                return null
+            }
         }
     }
 }
