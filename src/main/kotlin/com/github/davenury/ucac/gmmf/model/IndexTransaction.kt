dfa21eefa2ad799be161cf69ceedbe3af748a6b2
Kamil Jarosz <kjarosh256@gmail.com>
2025-03-14 15:18:54 +0100

    gmmf: Improve index tx error handling

--- src/main/kotlin/com/github/davenury/ucac/gmmf/model/IndexTransaction.kt
+++ src/main/kotlin/com/github/davenury/ucac/gmmf/model/IndexTransaction.kt
@@ -10,7 +10,6 @@ import com.github.kjarosh.agh.pp.index.VertexIndices
 import com.github.kjarosh.agh.pp.index.events.Event
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import kotlin.system.exitProcess
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
 @JsonSubTypes(
@@ -48,6 +47,7 @@ data class AcceptExternalEvent(val vertex: VertexId, val event: Event) : IndexTr
         indices: VertexIndices,
         eventDatabase: EventDatabase,
     ) {
+        logger.info("Applying accepting external event {}", event.id)
         eventDatabase.post(vertex, event)
         eventDatabase.acceptedEventIds.add(event.id)
     }
@@ -58,11 +58,11 @@ data class ProcessEventTx(val vertex: VertexId, val eventId: String, val diff: I
         indices: VertexIndices,
         eventDatabase: EventDatabase,
     ) {
+        logger.info("Applying processed event {}", eventId)
         val existingId = eventDatabase.getInbox(vertex).removeFirst().id
         if (existingId != eventId) {
-            logger.error("Event ID mismatch, expected $eventId, was $existingId")
-            // TODO better handling?
-            exitProcess(1)
+            logger.error("ProcessEventTx: Event ID mismatch, expected $eventId, was $existingId")
+            throw RuntimeException("ProcessEventTx: Event ID mismatch, expected $eventId, was $existingId")
         }
 
         diff.apply(indices)
@@ -82,11 +82,11 @@ data class SendOutboxEvent(val peersetId: PeersetId, val eventId: String) : Inde
         indices: VertexIndices,
         eventDatabase: EventDatabase,
     ) {
+        logger.info("Applying sent event {}", eventId)
         val existingId = eventDatabase.getOutbox(peersetId).removeFirst().second.id
         if (existingId != eventId) {
-            logger.error("Event ID mismatch, expected $eventId, was $existingId")
-            // TODO better handling?
-            exitProcess(1)
+            logger.error("SendOutboxEvent: Event ID mismatch, expected $eventId, was $existingId")
+            throw RuntimeException("SendOutboxEvent: Event ID mismatch, expected $eventId, was $existingId")
         }
     }
 }
